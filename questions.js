771bc70b0775bca0775c47f6684a64756e5af0d4
Asogba Abiodun Kendrick <asogbaabiodun92@gmail.com>
2024-02-13 12:04:02 +0100

    first commit

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text MarkUp Language ",
+    options: [
+      "A. Hyper Type Multi Language ",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text MarkUp Language ",
+      "D. Hyper Text Multi Language"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer:  "A. Cascading style sheet ",
+    options: [
+        "A. Cascading style sheet ",
+        "B. Cute style sheet ",
+        "C. Computer style sheet ",
+        "D. Codehal style sheet "
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "A. HyperText Preprocessor",
+    options: [
+        "A. HyperText Preprocessor",
+        "B. HomeText Programming",
+        "C. HyperText PreProgramming",
+        "D. Programming HyperText Preprocessor"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "D. Structured Query Language",
+    options: [
+        "A. Strength Query Language",
+        "B. Stylesheet Query Language",
+        "C. Science Question Language",
+        "D. Structured Query Language"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "D. Extensible Markup Language",
+    options: [
+        "A. Extensible Markup Language",
+        "B. Extensible Markup Language",
+        "C. Extensible Markup Language",
+        "D. Extensible Markup Language"
+    ]
+  }
+
+];
